1cd3c3e0a85b6c0ef89e8373532086e76ca5f3e3
ariobii <auliabimo5@gmail.com>
2019-03-19 14:47:33 +0700

    Sudah selesai buat kelas Mahasiswa dan 2 Objek

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace CobaClassObject
+﻿using System;
+
+namespace CobaClassObject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Mahasiswa Aulia;
+            //buat objek 
+            Mahasiswa Aulia = new Mahasiswa();
+            Mahasiswa Hasbi = new Mahasiswa();
+
+            //set nilai properties
+            Aulia.NIM = "18.11.2279";
+            Aulia.Nama = "Aulia Ariobimo";
+            Aulia.IPK = 4.5f;
+
+            Hasbi.NIM = "18.11.2252";
+            Hasbi.Nama = "Hasbi Ash Shiddiq";
+            Hasbi.IPK = 5.5f;
+
+            //memanggil method
+            Aulia.Registrasi();
+            Aulia.IsiKRS();
+
+            Hasbi.Registrasi();
+            Hasbi.IsiKRS();
+
+            Console.ReadKey();
         }
     }
 }
