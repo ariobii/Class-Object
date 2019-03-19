68bc6db84ae5c548da65b240dc708335fd0b2e2a
ariobii <auliabimo5@gmail.com>
2019-03-19 14:18:33 +0700

    inisiasi projek pertemuan pemrograman 3

---
+++ Mahasiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CobaClassObject
+{
+    public class Mahasiswa
+    {
+        //properties
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public float IPK { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM  : {0}", NIM);
+            Console.WriteLine("IPK : {0}", IPK);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        //method juga
+        public void IsiKRS()
+        {
+            Console.WriteLine("{0} sedang melakukan proses KRS\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CobaClassObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa Aulia;
+        }
+    }
+}
